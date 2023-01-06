443427274b6bb2a2d4e88b39cf98be865c504a8c
Wilson Syring <wilson.syring@gmail.com>
2023-01-06 16:08:15 -0600

    test push

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -21,7 +21,7 @@ public class MySQLUsersDao implements Users {
         }
     }
 
-
+//hello change
     @Override
     public User findByUsername(String username) {
         String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
