32c23afe967868dd13153867211788eb11ffc946
Wilson Syring <wilson.syring@gmail.com>
2023-01-12 14:00:37 -0600

    search functionality still not correct

--- src/main/java/com/codeup/adlister/controllers/SearchServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchServlet.java
@@ -13,10 +13,15 @@ import java.io.IOException;
 @WebServlet(name = "SearchServlet", urlPatterns = "/search")
 public class SearchServlet extends HttpServlet {
 
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("ads",req.getSession().getAttribute("ads"));
+        System.out.println(req.getSession().getAttribute("ads"));
+        req.getRequestDispatcher("WEB-INF/ads/index.jsp").forward(req, resp);
+    }
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String searchItem = req.getParameter("searchbox");
-        req.setAttribute("ads", DaoFactory.getAdsDao().adSearch(searchItem));
-        req.getRequestDispatcher("WEB-INF/ads/index.jsp").forward(req, resp);
+        req.getSession().setAttribute("ads", DaoFactory.getAdsDao().adSearch(searchItem));
+        System.out.println(searchItem);
+        resp.sendRedirect("/search");
     }
 }
